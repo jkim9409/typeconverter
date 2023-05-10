fbbc419d646c87a3abd3fec140da1c6db34daad3
jkim9409 <jkimnyc2020@gmail.com>
2023-05-10 20:38:08 +0900

    String to IpPort Class converter Test Codes
    String to IpPort Class converter Test Codes

--- typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,6 +1,6 @@
 package hello.typeconverter.converter;
 
-import org.assertj.core.api.Assertions;
+import hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -20,5 +20,20 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(ipPort);
+    }
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(ipPort);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
     }
 }
