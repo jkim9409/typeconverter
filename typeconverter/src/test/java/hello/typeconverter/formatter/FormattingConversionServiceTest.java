390e221c380f9b09518839d1640e377b9d5cfbcf
jkim9409 <jkimnyc2020@gmail.com>
2023-05-11 19:07:01 +0900

    test cases for formattingConversionService
    test cases for formattingConversionService

--- typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
+++ typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -8,6 +8,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.format.support.DefaultFormattingConversionService;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class FormattingConversionServiceTest {
 
     @Test
@@ -23,8 +25,13 @@ public class FormattingConversionServiceTest {
 
         //use converter
         IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
-        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.01", 8080));
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
 
         //use formatter
+        String convertString = conversionService.convert(1000, String.class);
+        assertThat(convertString).isEqualTo("1,000");
+
+        Long convertLong = conversionService.convert("1,000", Long.class);
+        assertThat(convertLong).isEqualTo(1000L);
     }
 }
