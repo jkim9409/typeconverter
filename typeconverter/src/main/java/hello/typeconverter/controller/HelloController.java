90366eab9469fc76f1e7759d773a55878cc41365
jkim9409 <jkimnyc2020@gmail.com>
2023-05-11 18:24:27 +0900

    ipPort function that can recieve IpPort Class as param
    ipPort function that can recieve IpPort Class as param

--- typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,14 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    //http://localhost:8080/ip-port?ipPort=127.0.0.1:8080
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort Port = " + ipPort.getPort());
         return "ok";
     }
 }
