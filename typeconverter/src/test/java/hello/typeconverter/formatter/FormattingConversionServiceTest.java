c639dfa50e30e4da5ba189ffcc334f68dbaae949
jkim9409 <jkimnyc2020@gmail.com>
2023-05-11 19:04:09 +0900

    DefaultFormattingConversionService
    DefaultFormattingConversionService can help you use converter and formatter at the same time

--- typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
+++ typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -3,6 +3,8 @@ package hello.typeconverter.formatter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.format.support.DefaultFormattingConversionService;
 
@@ -18,5 +20,11 @@ public class FormattingConversionServiceTest {
 
         //add formatter
         conversionService.addFormatter(new MyNumberFormatter());
+
+        //use converter
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.01", 8080));
+
+        //use formatter
     }
 }
